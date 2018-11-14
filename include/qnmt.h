4ac99b3bf98528219590b6c14c0900f285e6e49e
Christophe Servan <christophe.servan@gmail.com>
2018-11-14 19:24:47 +0100

    First test to link to a moses server

--- include/qnmt.h
+++ include/qnmt.h
@@ -22,6 +22,7 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 ***************************************************************************/
+#pragma once
 #include <iostream>
 #include <boost/locale.hpp>
 #include <boost/algorithm/string/replace.hpp>
