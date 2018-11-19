e24782458563727738a2d89a2d9d0d208c4ba346
Christophe Servan <christophe.servan@gmail.com>
2018-11-19 18:36:44 +0100

    Update

--- src/client_moses.cpp
+++ src/client_moses.cpp
@@ -5,6 +5,7 @@
 #include <xmlrpc-c/girerr.hpp>
 #include <xmlrpc-c/base.hpp>
 #include <xmlrpc-c/client_simple.hpp>
+#include <xmlrpc-c/xml.hpp>
 
 using namespace std;
 
@@ -17,13 +18,25 @@ int main(int argc, char **) {
 
     try {
         string const serverUrl("http://localhost:8080/RPC2");
-        string const methodName("translate");
+//         string const methodName("translate");
 
         xmlrpc_c::clientSimple myClient;
         xmlrpc_c::value result;
+        xmlrpc_c::paramList myParamList;
+//         myParamList.
+
+    string const callXml("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n <methodCall>\r\n  <methodName>translate</methodName>\r\n  <params>\r\n <param><text><s>Ceci est un text</s></text></param>\r\n </params>\r\n  </methodCall>\r\n");
+
+    string methodName;
+
+    xmlrpc_c::xml::parseCall(callXml, &methodName, &myParamList);
+
+    cout << "It is a call of method " << methodName
+         << "with " << myParamList.size() << endl;
         
-        myClient.call(serverUrl, methodName, "text", &result, "This is a test");
-        cerr << result <<endl;
+        
+        myClient.call(serverUrl, methodName, &myParamList, &result);
+//         cerr << result <<endl;
 
 //         int const sum = xmlrpc_c::value_int(result);
             // Assume the method returned an integer; throws error if not
