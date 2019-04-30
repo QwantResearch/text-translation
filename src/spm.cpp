fbb3a336625f281f85243ba6d396f73fb3252a8c
Christophe Servan <christophe.servan@gmail.com>
2019-04-30 13:46:44 +0200

    Add SentencePiece library as alternative BPE lib

---
+++ src/spm.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+* MIT License
+* 
+* Copyright (c) 2018 Christophe SERVAN, Qwant Research, 
+* email: christophe[dot]servan[at]qwantresearch[dot]com
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+***************************************************************************/
+#include "spm.h"
+
+spm::spm(std::string& modelpath)
+{
+    const auto status = _processor.Load(modelpath);
+    if (!status) 
+    {      
+        std::cerr << "Error while loading SentencePiece model: " << modelpath << std::endl;
+        exit(1);
+    }
+}
+
+std::vector< std::string > spm::segment(std::string& sentence)
+{
+    std::vector<std::string> to_return;
+    _processor.Encode(sentence, &to_return);
+}
